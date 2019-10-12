104ba2e6f0df90212acb9a4204aa89d6823bc7ae
admin <zacharyjsanders@gmail.com>
2019-10-12 15:45:37 -0600

    initial plugin setup

---
+++ wp-wit-slider.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Plugin Name:  Wit Slider
+Plugin URI:   https://github.com/endurain
+Description:  A slider built with Glider.js
+Version:      1.0
+Author:       Zac Sanders
+Author URI:   https://github.com/endurain
+License:      GPL2
+License URI:  https://www.gnu.org/licenses/gpl-2.0.html
+Text Domain:  wp-wit-slider
+Domain Path:  /languages
+*/
